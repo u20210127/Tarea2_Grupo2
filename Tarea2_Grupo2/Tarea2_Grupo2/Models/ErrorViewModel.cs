e0d946c793d35a47962ac2f6c89723b21af9c1cd
U20210127 <U20210127@univo.edu.sv>
2024-02-23 13:27:50 -0600

    Estructura para trabajar

---
+++ Tarea2_Grupo2/Tarea2_Grupo2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tarea2_Grupo2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
