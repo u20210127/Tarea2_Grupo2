18b03d3c47a2d40464685ca133f699a3d86627f1
Enmanuel12h <u20210506@univo.edu.sv>
2024-02-24 12:54:04 -0600

    actualizando codigo tabla Clientes

--- Tarea2_Grupo2/Tarea2_Grupo2/Models/CustomersModel.cs
+++ Tarea2_Grupo2/Tarea2_Grupo2/Models/CustomersModel.cs
@@ -1,6 +1,19 @@
-﻿namespace Tarea2_Grupo2.Models
+﻿using System.ComponentModel;
+
+namespace Tarea2_Grupo2.Models
 {
-    public class CustomerModel
+    public class CustomersModel
     {
-    }
+
+		public int Id_Clientes{ get; set; }
+
+		[DisplayName("Nombre_Cliente")]
+		public string Nombre_Cliente { get; set; }
+
+		[DisplayName("direccion cliente")]
+		public string Direccion_Cliente { get; set; }
+
+		[DisplayName("telefono cliene")]
+		public int Telefono_Cliente { get; set; }
+	}
 }
