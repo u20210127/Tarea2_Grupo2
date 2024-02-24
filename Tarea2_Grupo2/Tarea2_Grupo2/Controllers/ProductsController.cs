4c1549d778812f1beb0738c07a8b85b734b25725
YanethZG <jeanethzelaya56@gmail.com>
2024-02-24 09:45:07 -0600

    Estructura para la tabla Products

---
+++ Tarea2_Grupo2/Tarea2_Grupo2/Controllers/ProductsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tarea2_Grupo2.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
