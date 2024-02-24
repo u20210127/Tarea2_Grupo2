d496642a63674d95da76214e522275f3946ec0cb
u20210127 <u20210127@univo.edu.sv>
2024-02-24 13:35:46 -0600

    Actualizando datos de la tabla Suppliers

---
+++ Tarea2_Grupo2/Tarea2_Grupo2/Controllers/SuppliersController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tarea2_Grupo2.Controllers
+{
+    public class SuppliersController : Controller
+    {
+        private readonly ISuppliersRepository _suppliersRepository;
+
+        public SuppliersController(ISuppliersRepository suppliersRepository)
+        {
+            _suppliersRepository = suppliersRepository;
+        }
+
+        public ActionResult Index()
+        {
+            return View(_suppliersRepository.GetAll());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(SuppliersModel suppliers)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(suppliers);
+            }
+
+            _suppliersRepository.Add(suppliers);
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
